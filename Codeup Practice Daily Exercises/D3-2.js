3ea20c536621b76342916e4d0c97a8f75d89f74e
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-06 11:36:33 -0500

    Day 3 Exercise 2

---
+++ Codeup Practice Daily Exercises/D3-2.js
@@ -0,0 +1,43 @@
+//Execute the following statement in the Chrome JavaScript console and then follow the directions below.
+let sample = "Hello Codeup";
+//undefined
+
+//Use .length to find the number of characters in the string.
+sample.length
+//12
+
+//Try to make the sample string all upper or all lower case.
+sample.toUpperCase();
+//'HELLO CODEUP'
+
+//Update the variable sample via concatenation so that it contains "Hello Codeup Students".
+sample = sample + " Students"
+//'Hello Codeup Students'
+
+//Replace "Students" with "Class".
+sample = sample.replace('Students', 'Class');
+//'Hello Codeup Class'
+
+//Find the index of "c" using .indexOf(). What do you observe?
+sample.indexOf("c");
+
+// its a -1 because there is not lowercase c in that line of text????
+
+
+//Find the index of "C" using .indexOf().
+sample.indexOf('C');
+
+// it's 6 because it is the 6 postion in starting from 0
+
+
+//Retrieve a substring that contains only the word "Codeup" by using indexOf() and substring().
+sample.indexOf('C');
+
+sample.indexOf('p');
+
+sample.substring(6, 12);
+
+/**
+ * needed to find the index for both the start and finish of the word "Codeup" which was 6 and 11.
+ * needed to go one over so it would account for the p when building the substring
+ */
