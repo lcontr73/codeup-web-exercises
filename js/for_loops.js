fcdca52231c5d83347dccfdca0231c6bf87548fe
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-12 16:45:56 -0500

    loops exercise created files

---
+++ js/for_loops.js
@@ -0,0 +1,29 @@
+"use strict";
+
+(function () {
+    function showMultiplicationTable(num) {
+        for(var i = 1, j = 1; i <= 10; i++, j++){
+                console.log(num + " x " + i + " = " + num * i);
+            }
+    }
+    showMultiplicationTable(7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
