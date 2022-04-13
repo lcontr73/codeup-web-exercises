41781745e41ea35297573576539c2d0f8b1eafa8
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-13 15:28:57 -0500

    added notes

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,17 +4,56 @@
     // 2.Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to
     // continue prompting the user if they enter invalid input.
 
-    var number = parseFloat(prompt('Pick an Odd number between 1 and 50'));
 
+// //My solution
     while (true) {
-
+        var number = parseFloat(prompt('Pick an Odd number between 1 and 50'));
         if (number % 2 === 1 && number <= 50) {
             break;
         }
         alert('try again');
-    }
+        }
+
     alert('correct choice');
 
+    // Instructor solution
+//     function getOddNumber(max){
+//         var input = prompt('Input odd number less than ' + max);
+//
+//         var isInputInvalid = true
+//
+//         while (isInputInvalid) {
+//             // if its not valid so not od or over the max value
+//             if ((parseInt(input) % 2 !==0 && parseInt(input) <= max)) break;
+//             // it will ask the prompt again, and it will keep doing this until its correct.
+//             input = prompt('Input odd number less than ' + max);
+//         }
+//         return parseInt(input);
+//     }
+// getOddNumber(5);
+//
+//     function outputOddNumbers(max) {
+//         var userInput = getOddNumber(max); //calling the odd number function we just made that handles the user input.
+//
+//         // Add number to skip
+//         console.log('Number to skip: ' + userInput);
+//
+//         for (var i = 1; i < max; i++) {
+//             // ability to skip a number
+//
+//             // print that we skipped the number
+//             // output the odd numbers
+//
+//             if (i === userInput) {
+//                 console.log('Yikes! Skipping number:' + userInput)
+//                 continue;
+//             }
+//
+//             console.log('Here is an odd number: ' + i);
+//         }
+//     }
+//
+//     outputOddNumbers(50)
 
     /*
      3. Use a loop and the continue statement to output all the odd numbers between 1 and 50,
@@ -48,8 +87,9 @@ Here is an odd number: 47
 Here is an odd number: 49
 
      */
-
+    // My solution
     var count = 0;
+    var number = parseFloat(prompt('Pick an Odd number to skip between 1 and 50'));
     while (count < 50) {
         count++;
 
