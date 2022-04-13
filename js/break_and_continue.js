50d8ec83da38dd6adcc0adeb81c36fef9308f4ab
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-13 10:49:44 -0500

    problem 3 complete reversed console.log

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,10 +4,9 @@
     // 2.Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to
     // continue prompting the user if they enter invalid input.
 
-    var number;
+    var number = parseFloat(prompt('Pick an Odd number between 1 and 50'));
 
     while (true) {
-        number = parseFloat(prompt('Pick an Odd number between 1 and 50'));
 
         if (number % 2 === 1 && number <= 50) {
             break;
@@ -17,45 +16,50 @@
     alert('correct choice');
 
 
-    // 3. Use a loop and the continue statement to output all the odd numbers between 1 and 50,
-    // except for the number the user entered.
+    /*
+     3. Use a loop and the continue statement to output all the odd numbers between 1 and 50,
+    except for the number the user entered.
+    Number to skip is: 27
 
-    // var count = 0;
-    // while (count < 10) {
-    //     count++;
-    //     if (count === 5) continue;
-    //     console.log('count', count)
-    // }
-    //
-    // count = 0;
-    // do {
-    //     count++;
-    //     if (count === 3) break; //END the loop
-    //     console.log("second-count", count)
-    //
-    // } while (true)
+Here is an odd number: 1
+Here is an odd number: 3
+Here is an odd number: 5
+Here is an odd number: 7
+Here is an odd number: 9
+Here is an odd number: 11
+Here is an odd number: 13
+Here is an odd number: 15
+Here is an odd number: 17
+Here is an odd number: 19
+Here is an odd number: 21
+Here is an odd number: 23
+Here is an odd number: 25
+Yikes! Skipping number: 27
+Here is an odd number: 29
+Here is an odd number: 31
+Here is an odd number: 33
+Here is an odd number: 35
+Here is an odd number: 37
+Here is an odd number: 39
+Here is an odd number: 41
+Here is an odd number: 43
+Here is an odd number: 45
+Here is an odd number: 47
+Here is an odd number: 49
+
+     */
 
     var count = 0;
     while (count < 50) {
         count++;
 
-        if (count === number) ;
-        console.log('We skipped it: ', number)
+        if (count === number) {
+        console.log('Yikes! Skipping number: ', number)
         continue;
     }
-    console.log(count);
-
-    // var count = 0
-    // do {
-    //     count++;
-    //     if (count === 50) break; //END the loop
-    //     console.log("second-count", count)
-    //
-    // } while (true)
-
-
-
-
-
+        if (count % 2 !== 0){
+            console.log(count);
+        }
+    }
 
 })();
