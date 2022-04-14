8d3501a4e8dfa88b9e94fdeea2d35c3c1f129b9b
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-14 11:30:16 -0500

    array iterating exercises complete

--- js/iterating.js
+++ js/iterating.js
@@ -7,33 +7,52 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-var names = ["Bob", "Sara", "Tom", "Jane"];
+
+ var names = ['sara', 'bill', 'hank', 'dale']
+
+
+// var names = ["Bob", "Sara", "Tom", "Jane"];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
 console.log(names.length);
+
+
+
+
+
+
+
+    // console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-console.log(names);
+    // console.log(names[0]);
+    // console.log(names[1]);
+    // console.log(names[2]);
+    // console.log(names[3]);
+
+
+
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-for (var i = 0; i < names.length; i += 1) {
-    console.log(names[i]);
-}
+// for (var i = 0; i < names.length; i += 1) {
+//     console.log(names[i]);
+// }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-    names.forEach(function(element, index, array){
-        console.log(element);
+    names.forEach(function(name){
+        console.log(name);
     });
     /**
      * TODO:
@@ -64,24 +83,24 @@ for (var i = 0; i < names.length; i += 1) {
     }
     myPickLast(numbersList);
 
-    // function numbersLog1(numbersList) {
-    //     for (var i = 0; i < numbersList.length; i +=1) {
-    //         console.log(numbersList[0]);
-    //     }
-    // }
-    // console.log(numbersLog1(numbersList));
-    //
-    // function numbersLog2(numbersList) {
-    //     for (var i = 0; i < numbersList.length; i += 1) {
-    //         console.log(numbersList[1]);
-    //     }
-    // }
-    // console.log(numbersLog2(numbersList));
-    //
-    // function numbersLogLast(numbersList) {
-    //     for (var i = 0; i < numbersList.length; i +=1) {
-    //         console.log(numbersList[numbersList.length -1]);
-    //     }
-    // }
-    // console.log(numbersLogLast(numbersList));
+    function numbersLog1(numbersList) {
+        for (var i = 0; i < numbersList.length; i +=1) {
+            console.log(numbersList[0]);
+        }
+    }
+    console.log(numbersLog1(numbersList));
+
+    function numbersLog2(numbersList) {
+        for (var i = 0; i < numbersList.length; i += 1) {
+            console.log(numbersList[1]);
+        }
+    }
+    console.log(numbersLog2(numbersList));
+
+    function numbersLogLast(numbersList) {
+        for (var i = 0; i < numbersList.length; i +=1) {
+            console.log(numbersList[numbersList.length -1]);
+        }
+    }
+    console.log(numbersLogLast(numbersList));
 })();
