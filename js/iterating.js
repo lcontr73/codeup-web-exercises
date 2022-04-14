728b9728fc8907196f8b49e9a023a3c1e4c85acc
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-13 20:59:31 -0500

    add names array and for loop

--- js/iterating.js
+++ js/iterating.js
@@ -1,30 +1,33 @@
+"use strict";
 (function(){
-    "use strict";
+
 
     /**
      * TODO:
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+var names = ["Bob", "Sara", "Tom", "Jane"];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+console.log(names);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+for (var i = 0; i < names.length; i += 1) {
+    console.log(names[i]);
+}
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
