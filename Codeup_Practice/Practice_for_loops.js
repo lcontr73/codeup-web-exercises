7d630ebbb0048021761ef04c3a3e4c942c0ca06e
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-24 17:23:07 -0500

    Practice break and continue using a while loop problem 2.

--- Codeup_Practice/Practice_for_loops.js
+++ Codeup_Practice/Practice_for_loops.js
@@ -113,13 +113,26 @@
     /**
     Break and Continue
     1. Create a file named break_and_continue.js in the js directory.
+     **/
 
 
-     2. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+    /**
+    2. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+     **/
 
 
 
-     3. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+    while (true) {
+        var pickANumber = parseFloat(prompt('Pick a number between 1 and 50'));
+        if (pickANumber % 2 !== 0 && pickANumber <= 50 || pickANumber === '') {
+            break;
+        } else {
+            alert('Please try again.')
+        }
+    }
+
+    /**
+    3. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
         Your output should look like this:
 
 
