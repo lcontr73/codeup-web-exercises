7c397c074ba8cd4d09b23a69b0919a9d74fff5db
Leonard Contreras <leonard.contreras03@gmail.com>
2022-04-25 21:55:55 -0500

    practiced iterating arrays

--- Codeup_Practice/Practice_iterating.js
+++ Codeup_Practice/Practice_iterating.js
@@ -6,30 +6,37 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    var names = ['Bob', 'Sarah', 'Samantha', 'Ted'];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+names.forEach(function(name) {
+    console.log(name);
+})
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +50,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+var numbers = [1,2,3,4,5];
 
+function first(number) {
+    console.log(numbers[0]);
+}
+    first(numbers)
+
+function second(number) {
+    console.log(numbers[1]);
+}
+    second(numbers)
+
+function last(number) {
+    console.log(numbers[numbers.length - 1]);
+}
+    last(numbers);
 })();
