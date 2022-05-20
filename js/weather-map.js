1ade904bc7f9bdfa684c96c11d893701c929cc0a
Leonard Contreras <leonard.contreras03@gmail.com>
2022-05-20 11:45:52 -0500

    lecture notes

---
+++ js/weather-map.js
@@ -0,0 +1,8 @@
+// $.get("http://api.openweathermap.org/data/2.5/weather", {
+//     APPID: OPEN_WEATHER_APPID,
+//     q:     "San Antonio, US"
+// }).done(function(data) {
+//     console.log(data);
+// });
+
+
